1dbe78dba9e9486da33dc66001d9a8faeae48d03
Mojtaba Khooryani <m.khooryani@outlook.com>
2021-09-23 09:03:55 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NerdVision;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NV.Start("nv-vYbgfBZZXEjtvfQJ2cQ4");
+            int a = 5;
+            int b = a * 2;
+            int c = b * 10;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
